dfb8feff00a639f7a6710ddb2f85e7e6c67e3dbe
Virti7702 <97676077+Virti7702@users.noreply.github.com>
2022-01-14 15:01:25 +0530

    Add files via upload

---
+++ pratical2_2_d.py
@@ -0,0 +1,10 @@
+'''
+Name - Virti Shah
+ID - 20CE135
+Aim - A python program to convert a tuple in string.
+Github link - https://github.com/Virti7702/Practical2-2-d.git
+
+'''
+t=('a','b')
+str = ''.join(t)
+print(str)
